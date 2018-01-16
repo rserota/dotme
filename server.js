6141f71924eacab270df2ee3201a51f64f66c241
raphael.serota <raphael.serota@gmail.com>
2018-01-16 00:25:00 -0500

    almost a navbar

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ var sessionMiddleware = sessionsModule({
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('./public'))
-app.use(sessionMiddleware)
+// app.use(sessionMiddleware)
 
 app.use(require('./routes/mainRoutes'))
 
